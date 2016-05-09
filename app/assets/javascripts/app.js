b62f5b8149c3a8ff391c51564208f5c7ad996725
Adam Szczombrowski <adam.szczombrowski@gmail.com>
2016-05-09 11:23:56 +0200

    prepare directory structure for angular app

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,15 +1,31 @@
 (function(){
-var library = angular.module('library', ['templates', 'ngRoute']);
+var library = angular.module('library', ['templates', 'ngRoute', 'booksServices']);
 
 library.config(['$routeProvider', function($routeProvider){
   $routeProvider
   .when('/', {templateUrl: 'index.html'})
   .when('/books', {
     templateUrl: 'books/index.html',
-    controller: 'BooksController'})
+    controller: 'BooksController'
+  })
+  .when('/movies', {
+    templateUrl: 'movies/index.html',
+    controller: 'MoviesController'
+  })
+  .when('music', {
+    templateUrl: 'music/index.html',
+    controller: 'MusicController'
+  })
+  .otherwise({redirectTo: '/'});
 }]);
 
 library.controller('BooksController', ['$scope', function($scope){
   $scope.va = 'varvar'
+}]);
+library.controller('MusicController', ['$scope', function($scope){
+
+}]);
+library.controller('MoviesController', ['$scope', function($scope){
+
 }]);
 })();
---
+++ app/assets/javascripts/services/booksServices.js
@@ -0,0 +1,1 @@
+var booksServices = angular.module('booksServices', []);
