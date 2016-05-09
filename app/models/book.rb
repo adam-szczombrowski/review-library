8e248e42bc2ca2f35c01134458d962cb6ee451e1
Adam Szczombrowski <adam.szczombrowski@gmail.com>
2016-05-09 16:09:04 +0200

    fix book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ActiveRecord::Base
   belongs_to :user
-  validates :review, inclusion: {in: [1,2,3,4,5]}
+  validates :rating, inclusion: {in: [1,2,3,4,5]}
   validates :author, :review, :rating, :title, presence: true
 end
