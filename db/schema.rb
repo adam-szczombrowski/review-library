f1e6b2c47666f71fe7cf0bec16c10bb9afbbcba0
Adam Szczombrowski <adam.szczombrowski@gmail.com>
2016-05-10 15:33:23 +0200

    add image to book

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160509101655) do
+ActiveRecord::Schema.define(version: 20160510090519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20160509101655) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "title"
+    t.string   "image"
   end
 
   add_index "books", ["user_id"], name: "index_books_on_user_id", using: :btree
